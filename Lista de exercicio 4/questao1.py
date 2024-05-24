52ad0b51b5fdd6e7d9e4035c806753967013ef47
aninhatcdc <160986128+aninhatcdc@users.noreply.github.com>
2024-05-23 23:17:42 -0300

    Add files via upload

---
+++ Lista de exercicio 4/questao1.py
@@ -0,0 +1,7 @@
+metros= float(input('digite quantos metros você quer converter: '))
+
+centimetros=(metros)
+centimetros= metros * 100
+print(f'{metros} metros é igual a {centimetros} centímetros.')
+def metros_para_centimetros(metros):
+    return centimetros
